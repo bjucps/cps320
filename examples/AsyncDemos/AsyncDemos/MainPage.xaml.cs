341297f579a2bf39f34610d117b0de60b5c0674b
Stephen Schaub <sschaub@bju.edu>
2021-09-21 13:35:25 -0400

    Updates

---
+++ examples/AsyncDemos/AsyncDemos/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AsyncDemos
+{
+    public partial class MainPage : ContentPage
+    {
+
+        CancellationTokenSource cancelSource;
+        SearchHandler searchHandler = new SearchHandler();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LiveSearchEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (cancelSource != null)
+            {
+                cancelSource.Cancel();
+            }
+
+            cancelSource = new CancellationTokenSource();
+            
+            try
+            {
+                MyListView.ItemsSource = await searchHandler.SearchWithCancelAndFeedbackAsync(LiveSearchEntry.Text, 
+                    msg => {
+                            SearchProgressLabel.Text = msg;
+                            Console.WriteLine($"{DateTime.Now} - {msg}");
+                    },
+                    cancelSource.Token);
+            } catch (OperationCanceledException ex)
+            {
+                Console.WriteLine($"{DateTime.Now} - Operation cancelled");
+            }
+            
+
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (cancelSource != null)
+            {
+                cancelSource.Cancel();
+            }
+        }
+
+        private async void Search_Clicked(object sender, EventArgs e)
+        {
+            MyListView.ItemsSource = await searchHandler.SearchAsync(SearchEntry.Text);
+            MyListView.ItemsSource = await searchHandler.SearchWithFeedbackAsync(SearchEntry.Text,
+                (string msg) =>
+                {
+                    SearchProgressLabel.Text = msg;
+                });
+        }
+    }
+}
---
+++ examples/AsyncDemos/AsyncDemos/SearchHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemos
+{
+    class SearchHandler
+    {
+
+        public async Task<List<string>> SearchAsync(string text)
+        {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(SearchHandler)).Assembly;
+            var results = new List<string>();
+
+            using (var rd = new StreamReader(assembly.GetManifestResourceStream("AsyncDemos.mywords.txt")))
+            {
+                int count = 0;
+                string line = await rd.ReadLineAsync();
+                while (line != null)
+                {
+                    await Task.Delay(100);
+                    if (line.Contains(text))
+                    {
+                        results.Add(line);
+                    }
+                    line = await rd.ReadLineAsync();
+
+                    ++count;
+                    if (count % 5 == 0)
+                    {
+                        Console.WriteLine($"Searched {count} records...");
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public async Task<List<string>> SearchWithFeedbackAsync(string text, Action<string> progress) {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(SearchHandler)).Assembly;
+            var results = new List<string>();
+
+            int count = 0;
+            using (var rd = new StreamReader(assembly.GetManifestResourceStream("AsyncDemos.mywords.txt")))
+            {
+                string line = await rd.ReadLineAsync();
+                while (line != null)
+                {
+                    if (count % 5 == 0)
+                    {
+                        progress($"Searched {count} records...");
+                    }
+
+                    await Task.Delay(100);
+
+                    if (line.Contains(text))
+                    {
+                        results.Add(line);
+                    }
+                    line = await rd.ReadLineAsync();
+                    count++;
+                }
+            }
+
+            return results;
+        }
+
+        public async Task<List<string>> SearchWithCancelAndFeedbackAsync(string text, Action<string> progress, CancellationToken token)
+        {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(SearchHandler)).Assembly;
+            progress("Beginning Search");
+            var results = new List<string>();
+
+            int count = 0;
+            using (var rd = new StreamReader(assembly.GetManifestResourceStream("AsyncDemos.mywords.txt")))
+            {
+                string line = await rd.ReadLineAsync();
+                while (line != null)
+                {
+                    if (count % 5 == 0)
+                    {
+                        progress($"Searched {count} records...");
+                    }
+                    
+                    //await Task.Delay(10000, token);
+                    token.ThrowIfCancellationRequested();
+                    if (line.Contains(text))
+                    {
+                        results.Add(line);
+                    }
+                    line = await rd.ReadLineAsync();
+                    count++;
+                }
+            }
+
+            progress("Search ended");
+
+            return results;
+        }
+
+    }
+}
